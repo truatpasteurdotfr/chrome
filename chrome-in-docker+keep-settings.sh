2c2321a5c637b9ac337266929f6202ca825523bf
Tru Huynh <tru@centos.org>
2015-04-17 15:36:52 +0200

    fixing starting scripts --net host

--- chrome-in-docker+keep-settings.sh
+++ chrome-in-docker+keep-settings.sh
@@ -4,6 +4,8 @@ XAUTH=`mktemp`
 xauth nlist ${DISPLAY}| sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 docker run -t \
+    --net host \
+    --cpuset 0 \
     --memory 512mb \
  -v $XSOCK:$XSOCK \
  -v $XAUTH:$XAUTH \
